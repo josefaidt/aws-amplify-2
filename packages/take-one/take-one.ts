a19867845ceea5682296edccf613f55962d1fb9f
josefaidt <josef.aidt@gmail.com>
2024-05-29 18:05:23 -0700

    examples

--- packages/take-one/take-one.ts
+++ packages/take-one/take-one.ts
@@ -1,17 +1,19 @@
-import {
-  generateClient,
-  getUserAttributes,
-  getBucket,
-  upload,
-  signIn,
-} from "aws-amplify"
+import { generateClient, getUserAttributes, upload, signIn } from "aws-amplify"
 
 const client = generateClient()
+
+client.models.Todo.create({})
+// @ts-expect-error "Property 'DoesNotExist' does not exist on type ..."
+client.models.DoesNotExist.create({})
 
 upload("monthlyreports", {
   path: "/raw",
   file: new File([], "my-file"),
 })
+
+signIn("Discord")
+// @ts-expect-error Argument of type '"LinkedIn"' is not assignable to parameter of type '"GitHub" | "Discord"'.
+signIn("LinkedIn")
 
 const attributes = getUserAttributes()
 
