7bfa76b685cf5cb07bbde373955e3b9450c3b14b
josefaidt <josef.aidt@gmail.com>
2024-06-12 16:26:22 -0700

    updates

--- packages/tmp-env/env.ts
+++
@@ -1,8 +0,0 @@
-import type { Infer } from "superstruct"
-import { AmplifyStorageOutputs } from "./lib/schema/AmplifyStorageOutputs"
-
-export type _Storage<T = Record<string, unknown>> = T extends Infer<
-  typeof AmplifyStorageOutputs
->
-  ? T
-  : never
--- packages/tmp-env/lib/schema/AmplifyDataOutputs.ts
+++
@@ -1,3 +0,0 @@
-import { object } from "superstruct"
-
-export const AmplifyDataOutputs = object({})
